43ebe23484cfb77a9fab46e294f19d97edbe5066
BILON Ikram <bilonikram20@gmail.com>
2023-12-19 00:41:08 +0100

    changes ..

--- src/main/java/folder/servlets/Login.java
+++ src/main/java/folder/servlets/Login.java
@@ -47,7 +47,7 @@ public class Login extends HttpServlet {
 		user.setPhoneU(request.getParameter("phoneU"));
 		user.setEmailU(request.getParameter("emailU"));
 		user.setLoginU(request.getParameter("loginU"));
-		user.setPwdU(request.getParameter("motPasseU"));
+		user.setPwdU(request.getParameter("password"));
 		user.setTypeU("Client");
 		
 		userDAO.create(user);
